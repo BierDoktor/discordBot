d98ddcc3b4c49ac208387b69fb49806bfafd1cc0
henrik <henrik.moelders@gmail.com>
2023-08-28 16:35:35 +0200

    fuck git

--- dist/reactionFactory/reactionList.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-module.exports = {
-    jonasReaction: { active: true, targetUserId: process.env.henrik, reactions: ['🇦', '🇷', '🇸', '🇨', '🇭', '🇫', '🇴', '🇹', '🇿', '🇪'] },
-    paulReaction: { active: true, targetUserId: process.env.paul, reactions: ['☕'] }
-};
--- dist/reactionFactory/reactions/jonasReaction.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jonasReaction = void 0;
-class jonasReaction {
-    targetUserId = process.env.jonas;
-    reactions = ['🇦', '🇷', '🇸', '🇨', '🇭', '🇫', '🇴', '🇹', '🇿', '🇪'];
-}
-exports.jonasReaction = jonasReaction;
